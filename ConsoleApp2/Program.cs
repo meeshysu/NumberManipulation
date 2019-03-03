03596eb642bfa5458e3a0ab7b1e6d17db7148e8c
Michelle Beshears <meeshysu@gmail.com>
2019-03-03 15:15:02 -0600

    finished projected pushed

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,39 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // one variable set to 1
+            // another variable to hold the separate number in order to multiply
+            // split in order to separate it from it's comma
+            // int32
+
+            Console.WriteLine("Type in some numbers, seprated with a comma, to do some multiplying!");
+            var x = Console.ReadLine();
+            Console.WriteLine("If you want to multiply, please type X, if you would like the Square Root, please type S."); 
+            var selectedMathChoice = Console.ReadLine().ToLower();
+            var valueToSplit = x.Split(',');
+            var y = 1;
+                if (selectedMathChoice == "x")
+                {
+                    foreach (var z in valueToSplit)
+                    {
+                        y = y * Convert.ToInt32(z);
+                    }
+                    Console.WriteLine("Your multiplied answer is {0}", y);
+                    Console.ReadLine();
+                }
+                else if (selectedMathChoice == "s")
+                {
+                    var emptyString = " ";
+                    foreach (var z in valueToSplit)
+                    {
+                        var a = 1;
+                        a = a * Convert.ToInt16(z);
+                        var squared = Math.Pow(a, 2);
+                        emptyString += squared.ToString() + ",";
+                    }
+                    Console.WriteLine("Your squared answer is {0}", emptyString.TrimEnd(','));
+                    Console.ReadLine();
+                }
         }
     }
 }
